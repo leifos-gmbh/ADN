4ff95e1a62c0edd312164aa82763fd666d803792
Alex Killing <alex.killing@gmx.de>
2017-09-15 14:27:58 +0200

    fixed include

--- Services/ADN/ES/classes/class.adnCertifiedProfessional.php
+++ Services/ADN/ES/classes/class.adnCertifiedProfessional.php
@@ -981,6 +981,7 @@ class adnCertifiedProfessional extends adnDBBase
 				include_once("./Services/ADN/EP/classes/class.adnAnswerSheet.php");
 				include_once("./Services/ADN/EP/classes/class.adnExaminationEvent.php");
 				include_once "Services/ADN/EP/classes/class.adnAssignment.php";
+				include_once "Services/ADN/EP/classes/class.adnAnswerSheetAssignment.php";
 				foreach (adnAssignment::getAllAssignments(array("user_id" => $this->getId())) as $ass)
 				{
 					$exam_event  = new adnExaminationEvent($ass["ep_exam_event_id"]);
