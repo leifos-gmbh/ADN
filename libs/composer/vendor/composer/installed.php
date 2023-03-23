9eb4fe8c707015348d5dfd297c30de3f8281aca3
smeyer <smeyer.ilias@gmx.de>
2023-03-23 19:41:13 +0100

    autload

--- libs/composer/vendor/composer/installed.php
+++ libs/composer/vendor/composer/installed.php
@@ -1,11 +1,11 @@
 <?php return array(
     'root' => array(
-        'pretty_version' => 'dev-adn_7',
-        'version' => 'dev-adn_7',
+        'pretty_version' => 'dev-adn_7_card',
+        'version' => 'dev-adn_7_card',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../../../',
         'aliases' => array(),
-        'reference' => 'b58772bc1182047b997feb46b0defbe273464743',
+        'reference' => '5cda7a7c4322fb8843972d1a29c707ebb76a5b93',
         'name' => 'ilias/ilias',
         'dev' => false,
     ),
@@ -83,12 +83,12 @@
             'dev_requirement' => false,
         ),
         'guzzlehttp/guzzle' => array(
-            'pretty_version' => '6.5.7',
-            'version' => '6.5.7.0',
+            'pretty_version' => '7.4.4',
+            'version' => '7.4.4.0',
             'type' => 'library',
             'install_path' => __DIR__ . '/../guzzlehttp/guzzle',
             'aliases' => array(),
-            'reference' => '724562fa861e21a4071c652c8a159934e4f05592',
+            'reference' => 'e3ff079b22820c2029d4c2a87796b6a0b8716ad8',
             'dev_requirement' => false,
         ),
         'guzzlehttp/promises' => array(
@@ -110,12 +110,12 @@
             'dev_requirement' => false,
         ),
         'ilias/ilias' => array(
-            'pretty_version' => 'dev-adn_7',
-            'version' => 'dev-adn_7',
+            'pretty_version' => 'dev-adn_7_card',
+            'version' => 'dev-adn_7_card',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../../../',
             'aliases' => array(),
-            'reference' => 'b58772bc1182047b997feb46b0defbe273464743',
+            'reference' => '5cda7a7c4322fb8843972d1a29c707ebb76a5b93',
             'dev_requirement' => false,
         ),
         'imsglobal/lti' => array(
@@ -309,6 +309,12 @@
             'aliases' => array(),
             'reference' => '2dfb5f6c5eff0e91e20e913f8c5452ed95b86621',
             'dev_requirement' => false,
+        ),
+        'psr/http-client-implementation' => array(
+            'dev_requirement' => false,
+            'provided' => array(
+                0 => '1.0',
+            ),
         ),
         'psr/http-factory' => array(
             'pretty_version' => '1.0.1',
